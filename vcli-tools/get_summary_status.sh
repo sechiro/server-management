24817ddfeb4a1e5ea024a0ccec6c1d9cb7188575
Seiichiro, Ishida <ishida.seiichiro@gmail.com>
2013-08-07 10:51:46 +0900

    Modify debug output

--- vcli-tools/get_summary_status.sh
+++ vcli-tools/get_summary_status.sh
@@ -3,14 +3,14 @@ set -ue
 export PERL_LWP_SSL_VERIFY_HOSTNAME=0
 
 VCLI_PATH=/usr/lib/vmware-vcli/apps
-_DEBUG=0
+_DEBUG=${_DEBUG:-"0"}
 user=readonly_user
 password=password
 
 usage(){
     local _line_index=$(( ${#BASH_LINENO[@]} - 2 ))
     if [ $_DEBUG = 1 ];then
-        echo "[`basename $0`: LineNo: ${BASH_LINENO[$_line_index]}] Usage: ./`basename $0` [-m mode(summary, network, all)] esx_hostname" 1>&2
+        echo -n "[`basename $0`: LineNo: ${BASH_LINENO[$_line_index]}] " 1>&2
     fi
     echo "Usage: ./`basename $0` [-m mode(summary, network, all)] esx_hostname" >&2
     exit 1
